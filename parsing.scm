67adca6bf86859fd944f539afed72fcf4fca60f1
KenDickey <Ken.Dickey@whidbey.com>
2016-06-28 06:34:30 -0700

    more mumble

--- parsing.scm
+++ parsing.scm
@@ -4,7 +4,7 @@
 ;;; DATE: 10 May 2016
 
 
-(define token-tag (cons token nil)) ;; not eq? to anyhthing else
+(define token-tag (cons 'token '())) ;; not eq? to anyhthing else
 
 (define (token? thing)
   (and (vector? thing)
@@ -33,9 +33,43 @@
 ;;  ...
 
 (define token-kinds
-;; NB: never 'keyword ; see scan-id-or-keyword
-  '( assignment carrot cascade colon badToken blockArg blockStart blockEnd comment eof keyword leftParen period rightParen string verticalBar whitespace )
+;; NB: token-kind is never 'keyword
+;; Keywords are recognized by caller (parser).
+;; Let parser deal with <identifier> <colon>
+;;  'foo:= 3'
+;; is    'foo := 3'
+;; NEVER 'foo: = 3'
+;; The scanner would need to look 2 chars ahead
+;; to resolve this, but the parser only needs
+;; one token (<colon> vs <whitespace> ..)
+  '( assignment
+     badToken blockArg blockStart blockEnd braceBegin braceEnd
+     carrot cascade characterLiteral colon comment
+     dynamicDictStart
+     eof
+     keyword
+     leftParen litArrayStart litByteArrayStart
+     minus
+     period
+     rightParen
+     sharp string symbol
+     verticalBar
+     whitespace )
 )
+
+(define (tokenizer-for-string aString)
+  (token-parser-for-port
+     (open-input-string aString)
+     aString ;; source
+     0
+     0))
+
+(define (tokenizer-for-file-named fileNameString)
+  (token-parser-for-port
+     (open-input-file aString)
+     aString ;; source
+     0
+     0))
 
 ;;; TOKEN-PARSER-FOR-PORT returns a function/thunk
 ;;; which returns tokens
@@ -43,14 +77,15 @@
 ;; Numbers, literal arrays, symbols, ... are NOT created
 ;; here, but by the caller of this function
 (define (token-parser-for-port inport source line column)
-  (let* ( (line line)   ;; May start at position > 0.
-          (col  column) ;; Likewise..
+  (let* ( (line    line)   ;; May start at position > 0.
+          (column  column) ;; Likewise..
           (buffer  (make-string 1024 #\space))
           (buf-len (string-length buffer))
           (token-len      #false)
           (token-location #false)
           (first-char     #false)
           (next-char      #false)
+          (port-closed    #false)
        )
 
     (define (add-to-buffer char)
@@ -61,18 +96,22 @@
       (set! token-len 0)
       ;; location is start of scanned token
       (set! token-location (vector source line column))
-      (set! first-char (read-char inport))
-      (add-to-buffer first-char)
+      (set! first-char (read-next-char))
+      (unless (eof-object? first-char)
+        ;; Can't store eof in a string.
+        (add-to-buffer first-char)) 
 
       (cond
        ((eof-object? first-char)
+        ;; (close-port port)
+        ;; (set! port-closed #t)
         (new-token 'eof)
         )
        ((whitespace? first-char)
         (consume-whitespace)
         )
        ((letter? first-char)
-        (scan-id-or-keyword)
+        (scan-identifier 'identifier)
         )
        ((digit? first-char)
         (scan-number)
@@ -81,7 +120,7 @@
         (case first-char
           ((#\") (consume-comment)) 
           ((#\#) (scan-sharp-literal))
-          ((#\') (scan-string))
+          ((#\') (scan-string 'string))
           ((#\:) (scan-colon))
           ((#\$) (scan-character-literal))
           ((#\[) (new-token 'blockStart))
@@ -92,6 +131,7 @@
           ((#\;) (new-token 'cascade))
           ((#\.) (new-token 'period))
           ((#\^) (new-token 'carrot))
+          ((#\-) (new-token 'minus))
           (else  (scan-binary-selector-or-unexpected))
           )
         )))
@@ -111,15 +151,22 @@
     ;; track line & col
     ;; peek ahead
     (define (read-next-char)
-      (let ( (char (read-char inport)) )
-        (if (newline-char? char)
-          (begin
-            (set! line (+ 1 line))
-            (set! col 0))
-          (set! col (+ 1 col)))
-        (set! next-char (peek-char inport))
-        char))
- 
+      (if port-closed
+          (eof-object)
+          (let ( (char (read-char inport)) )
+            (if (eof-object? char)
+                (begin
+                  (close-port inport)
+                  (set! port-closed #true))
+                (begin
+                  (if (char=? char #\newline)
+                      (begin
+                        (set! line (+ 1 line))
+                        (set! column 0))
+                      (set! column (+ 1 column)))
+                  (set! next-char (peek-char inport))))
+            char)))
+
     ;; make a new token using buffer for string
     (define (new-token kind)
       (let ( (token
@@ -130,42 +177,91 @@
         (set! token-len 0)
         token))
 
-    
     (define (consume-whitespace) ;; and return token
       (let loop ()
-        (if (whitespace? current-char)
+        (if (whitespace? next-char)
             (begin
-              (next-char)
+              (next-char-skip)
               (loop))
             (new-token 'whitespace))))
 
-    (define (scan-id-or-keyword)
+    (define (scan-identifier token-kind)
+      (let loop ()
+        (if (or (char-alphabetic? next-char)
+                (char-numeric? next-char))
+            (begin
+              (next-char-keep)
+              (loop))))
+      (new-token token-kind))
+
+    (define (scan-symbol)
+      (let loop ()
+        (if ;; identifier plus $:
+         (or (char-alphabetic? next-char)
+             (char-numeric?    next-char)
+             (char=?      #\:  next-char))
+         (begin
+           (next-char-keep)
+           (loop))))
+      (new-token 'symbol))
+
+    (define (scan-number)
+      ;;@@FIXME: NYI
+      #false
+      )
+
+    (define (consume-comment)
       (let loop ()
         (cond
-         ((or (char-alphabetic? next-char)
-              (char-numeric? next-char))
+         ((char=? #\" next-char)
+          (new-token 'comment)
+         )
+         (else
+          (next-char-skip)
+          (loop)))
+      ) )
+      
+    (define (scan-string token-kind)
+      (let loop ()
+        (cond
+         ((char=? #\' next-char)
           (next-char-keep)
-          (loop))))
-;;; Keywords are recognized by caller (parser).
-;;  (if (char=? #\: next-char)
-      ;; DO NOT READ the $:
-      ;; -- let parser deal with it
-      ;;  'foo:= 3'
-      ;; is    'foo := 3'
-      ;; NEVER 'foo: = 3'
-      (new-token 'identifier)) ;; NB: NEVER keyword
+          (if (char=? #\' next-char) ;; "...''... "
+              (begin
+                (next-char-keep)
+                (loop))
+              (new-token token-kind))
+          )
+         (else
+          (next-char-keep)
+          (loop)))
+      ) )
 
-    (define (scan-number) ;;@@FIXME: NYI
-      )
-
-    (define (consume-comment);;@@FIXME: NYI
-      )
-
-    (define (scan-sharp-literal);;@@FIXME: NYI
-      )
-
-    (define (scan-string) ;;@@FIXME: NYI
-      )
+    (define (scan-sharp-literal)
+      ;; Symbol, Literal Array or ByteArray,
+      ;; Dynamic Dictionary
+      (cond
+       ((char-alphabetic? next-char)
+        (scan-symbol)
+        )
+       ((char=? #\' next-char)
+        (scan-string 'symbol)
+        )
+       ((char=? #\( next-char)
+        (next-char-keep)
+        (new-token 'litArrayStart)
+        )
+       ((char=? #\[ next-char)
+        (next-char-keep)
+        (new-token 'litByteArrayStart)
+        )
+       ((char=? #\{ next-char)
+        (next-char-keep)
+        (new-token 'dynamicDictStart)
+        )
+       (else
+        (new-token 'sharp)) ;; probably an error!
+    ) )
 
     (define (scan-colon)
       ;; colon, assignment, blockArg
@@ -175,20 +271,21 @@
         (new-token 'assignment)
         )
        ((char-alphabetic? next-char)
-        (let loop ()
-          (cond
-           ((or (char-alphabetic? next-char)
-              (char-numeric? next-char))
-            (scan-char-keep))
-           (else
-            (new-token 'blockArg))))
+        (scan-identifier 'blockArg)
         )
        (else
-        (new-token 'colon))
+        (new-token 'colon)
+        )
      ) )
 
-     (define (scan-binary-selector-or-unexpected) ;; @@NYI
-       )
+    (define (scan-character-literal) 
+      (next-char-keep)
+      (new-token 'characterLiteral)
+      )
+
+    (define (scan-binary-selector-or-unexpected) ;; @@NYI
+      #false
+      )
 
   next-token ;; return the access function
 ) )
