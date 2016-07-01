2c1bd92052eab4444df7deaab404aeb678f36a82
KenDickey <Ken.Dickey@whidbey.com>
2016-06-30 21:41:36 -0700

    cleanup

--- apropos.scm
+++ apropos.scm
@@ -1,6 +1,18 @@
+;;; FILE: "apropos.scm"
+;;; IMPLEMENTS: apropos function for Scheme globals
+;;; AUTHOR: Ken Dickey
+;;; DATE: 30 June 2016
+
+;; Run from command line via "larceny -r7rs", then
+;;   (import (scheme load))
+;;   (load "apropos.scm")
+;;
+;; (apropos 'cons)
+;; (apropos "char")
+
 (import (primitives interaction-environment environment-variables)
         (srfi :13) ;; string library
-        (rnrs sorting)
+	(rnrs sorting)
         )
 
 (define (apropos string-or-symbol)
@@ -23,8 +35,16 @@
 (define (funky-symbol? s)
   (let ( (str (symbol->string s)) )
     (and (> (string-length str) 0)
-         (not (char-alphabetic? (string-ref str 0))))))
+         (not (ascii-letter? (string-ref str 0))))))
 
 (define (symbol<? s1 s2)
   (string<? (symbol->string s1)
             (symbol->string s2)))
+
+(define (ascii-letter? char)
+  (if (char? char) ;; eof protect
+      (let ( (charcode (char->integer char)) )
+        (or (<= 97 charcode 122)  ;; a..z
+            (<= 65 charcode  90))) ;; A..Z
+      #false))
+
