0877d98b02a901a094ff1cfe3cc3106089584a56
KenDickey <Ken.Dickey@whidbey.com>
2018-04-28 17:11:13 +0000

    added

---
+++ gambit-macros.scm
@@ -0,0 +1,19 @@
+;; Gambit macros
+
+(define-macro (unless test . body)
+  `(if ,test #f (begin ,@body)))
+
+(define-macro (when test . body)
+  `(if ,test (begin ,@body)))
+
+(define-macro (import . ignored) #f) ;; ignore R7RS imports
+
+(define-macro (capture-condition form) ;; used in st-conditions-tests.scm
+  `(call/cc
+    (lambda (exit)
+      (with-exception-handler
+       (lambda (c) (exit c))
+       (lambda ()  ,form))))
+)
+
+
